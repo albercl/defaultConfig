fb27b0cef7fad83dea4d7ced09ecde9e95b1a2e7
albercl <alberto.cuadradol@outlook.es>
2020-10-28 17:17:57 +0100

    Release 1.0

--- src/main/java/es/albercl/defaultConfig/DefaultConfig.java
+++ src/main/java/es/albercl/defaultConfig/DefaultConfig.java
@@ -1,7 +1,27 @@
 package es.albercl.defaultConfig;
 
-public class DefaultConfig {
-    public DefaultConfig() {
+import java.io.*;
+import java.util.Properties;
 
+public class DefaultConfig extends Properties {
+    public DefaultConfig(String defaultConfig, String config) throws IOException {
+        InputStream defaultStream = getClass().getResourceAsStream(defaultConfig);
+        File configFile = new File(config);
+
+        load(defaultStream);
+
+        if(!configFile.exists())
+            if(!configFile.createNewFile())
+                throw new IOException("Cannot create ' + config + ' file");
+
+        if(!configFile.canWrite())
+            throw new IOException("Cannot write '" + config + "' file");
+
+        if(!configFile.canRead())
+            throw new IOException("Cannot read '" + config + "' file");
+
+        load(new FileInputStream(configFile));
+
+        store(new FileOutputStream(configFile), null);
     }
 }
